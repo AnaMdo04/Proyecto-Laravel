1f294a9e8889eece36aa61e58c64155be0818830
Ana Montes de Ory <amonory327@g.educaand.es>
2024-03-08 21:05:25 +0100

    Solucionado Problema EventoSeeder

--- database/seeders/EventoSeeder.php
+++ database/seeders/EventoSeeder.php
@@ -7,25 +7,24 @@ use Illuminate\Database\Seeder;
 
 class EventoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     * Ejecuta los seeders de la base de datos.
-     */
-
     public function run()
     {
-        // Crea 5 eventos ficticios utilizando el factory de Evento
+        // Crear 5 eventos utilizando el factory
 
         \App\Models\Evento::factory(5)->create();
-
-        // Obtiene todos los eventos y, para cada uno, relaciona juegos aleatorios
-
-        \App\Models\Evento::all()->each(function ($evento) {
-
-            // Obtiene juegos aleatorios y los asocia al evento
-
-            $juegos = \App\Models\Juego::inRandomOrder()->take(rand(1, 5))->pluck('idJuego');
-            $evento->juegos()->attach($juegos);
-        });
     }
 }
+
+// Asignar juegos aleatorios a cada evento
+// Obtener todos los eventos y para cada uno, asignar de 1 a 5 juegos aleatorios
+
+\App\Models\Evento::all()->each(function ($evento) {
+
+    // Obtener de forma aleatoria entre 1 y 5 juegos y obtener sus IDs
+
+    $juegos = \App\Models\Juego::inRandomOrder()->take(rand(1, 5))->pluck('id');
+
+    // Asignar los juegos obtenidos al evento actual
+
+    $evento->juegos()->attach($juegos);
+});
